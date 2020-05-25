b6fb0c073a6698586ec6d1a81cd16e499617eee3
HECTOR HURTADO <hector.hurtado@pucese.edu.ec>
2020-05-25 11:40:01 -0500

    Agregar archivos de proyecto.

---
+++ App_Console3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace AppCons_test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //Ejemplo de los participantes de los pilares de la programación estructurada 
+            //secuencias
+
+            // int a;
+            // a = 10;
+            //  int b = 20;
+            //   Console.WriteLine("La multiplicación de {0} x {1} es {2} ", a, b, (a * b));
+            //    Console.WriteLine("Pulse una tecla para continuar...");
+            // Console.ReadKey();//
+
+            for (int x = 1; x <= 5; x++)
+
+            {
+
+                Console.WriteLine(x);
+
+            }
+            Console.ReadKey();
+
+            {
+
+                for (int x = 1; x <= 5; x++)
+                {
+                    for (int y = 1; y <= 5; y++)
+                    {
+                        Console.WriteLine("Hola Mundo");
+                    }
+
+
+                    Console.WriteLine();
+                }
+
+                Console.ReadKey();
+            }
+
+            {
+
+                string nombre = "HolaMundo2";
+
+                foreach (char palabra in nombre)
+                {
+
+                    Console.WriteLine(" " + palabra + " ");
+
+                }
+
+                Console.ReadKey();
+            }
+        }
+
+
+    }
+}
